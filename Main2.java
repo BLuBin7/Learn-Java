68404437efa25efd42523d65b324a6a518e3fdb0
BLuBin7 <builuongbinh7@gmail.com>
2023-02-02 12:17:26 +0700

    #Java 48 : startWidth

--- Main2.java
+++ Main2.java
@@ -91,16 +91,20 @@ public class Main2
         // Dương : >
         // Không : =
         // str1.compareTo(str2) : str 1 có  > < = str2 không
-        System.out.println("s1 compareTo s2 : " + s1.compareTo(s2));
+        // System.out.println("s1 compareTo s2 : " + s1.compareTo(s2));
         
-        //  Hàm compareToIgnoreCase : không phân biệt chữ hoa
-        System.out.println("s1 compareToIgnoreCase s2 : " + s1.compareToIgnoreCase(s2));
+        // //  Hàm compareToIgnoreCase : không phân biệt chữ hoa
+        // System.out.println("s1 compareToIgnoreCase s2 : " + s1.compareToIgnoreCase(s2));
         
-        // Hàm regionMatches : so sánh một đoạn
-        // regionMatches(in hoa hay không ,từ , chuỗi , đến , độ dài)
-        boolean ss = s1.regionMatches(false, 0, s2, 0, 1);
-        System.out.println("so sanh s1 tu 0 den 1 voi s2 : " + ss);
+        // // Hàm regionMatches : so sánh một đoạn
+        // // regionMatches(in hoa hay không ,từ , chuỗi , đến , độ dài)
+        // boolean ss = s1.regionMatches(false, 0, s2, 0, 1);
+        // System.out.println("so sanh s1 tu 0 den 1 voi s2 : " + ss);
 
+        // Hàm startWidth : kiểm tra chuỗi bắt đầu bằng 1 chuỗi do mình quy định sẵn
+        String sdt ="012345";
+        System.out.println(sdt.startsWith("012"));
+        System.out.println(sdt.startsWith("013"));
     }   
     // Java 59. Kiểu dữ liệu Enum trong lập trình Java
     // Enum là kiểu tập hợp nhiều class hữu hạn
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,6 +1,9 @@
- // System.out.println("s1 equals s3 " + s1.equals(s3));
-        // System.out.println("s1 equals s2 " + s1.equals(s2));
+System.out.println("s1 compareTo s2 : " + s1.compareTo(s2));
         
-        // // hàm equalsIgnoreCase : so sánh 2 chuỗi ( không phân biệt chữ hoa)
-        // System.out.println("s1 equalsIgnoreCase s2 " + s1.equalsIgnoreCase(s2));
+        // //  Hàm compareToIgnoreCase : không phân biệt chữ hoa
+        // System.out.println("s1 compareToIgnoreCase s2 : " + s1.compareToIgnoreCase(s2));
         
+        // // Hàm regionMatches : so sánh một đoạn
+        // // regionMatches(in hoa hay không ,từ , chuỗi , đến , độ dài)
+        // boolean ss = s1.regionMatches(false, 0, s2, 0, 1);
+        // System.out.println("so sanh s1 tu 0 den 1 voi s2 : " + ss);
