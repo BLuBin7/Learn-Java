ca2681a528b25d7547b5474d0d1fca6bef182e60
BLuBin7 <builuongbinh7@gmail.com>
2023-02-02 12:12:37 +0700

    #Java 48 : regionMatches

--- Main2.java
+++ Main2.java
@@ -96,6 +96,11 @@ public class Main2
         //  Hàm compareToIgnoreCase : không phân biệt chữ hoa
         System.out.println("s1 compareToIgnoreCase s2 : " + s1.compareToIgnoreCase(s2));
         
+        // Hàm regionMatches : so sánh một đoạn
+        // regionMatches(in hoa hay không ,từ , chuỗi , đến , độ dài)
+        boolean ss = s1.regionMatches(false, 0, s2, 0, 1);
+        System.out.println("so sanh s1 tu 0 den 1 voi s2 : " + ss);
+
     }   
     // Java 59. Kiểu dữ liệu Enum trong lập trình Java
     // Enum là kiểu tập hợp nhiều class hữu hạn
